fc303d4fbc757491593a1a33792b2c387e1c418c
evan.admin <evan.muzzall@berkeley.edu>
2018-11-09 13:27:28 -0800

    added Part 4 script

---
+++ Part 4 script.R
@@ -0,0 +1,40 @@
+# Day 4
+
+# Part 1 - mtcars
+
+# 1. Load the mtcars dataset.
+
+# 2. What are some useful functions to learn more about the data?
+
+# 3. Might you surmise a relationship about something like engine size and miles per gallon?
+
+# 4. Create boxplots for mpg as parsed by cyl.
+
+# 5. What does cor.test reveal about mpg and cyl?
+
+# 6. Create a scatterplot of the “mpg” and “hp” variables using ggplot(). What do you notice?
+
+# 7. Can cyl be used to predict mpg in a linear regression model? Why must you learn about the assumptions of a statistical test before applying it (or any other statistical test)?
+
+
+
+# Part 2 - gapminder
+
+# 1. Now that you are warmed up, load the “gapminder-FiveYearData.csv” file for yet another dataset you have never seen before. Save it in a variable named gap:
+
+# 2. How would you learn more about this dataset?
+
+# 3. Accomplish the following tasks:
+
+# 3A) Write three sentences describing this dataset and write one sentence that proposes a research question to be investigated.
+
+# 3B) Turn your research question into a testable hypothesis:
+
+# 3C) Compute descriptive statistics for each variable in the dataset.
+
+# 3D) Can you visualize the data from every single column in this dataset? Create a histogram, boxplot, or scatterplot for the ones you can, and explain in writing the ones you cannot.
+
+# 3E) Do these plots inform you about anything specific that fits your hypothesis and that you can justifiably test?
+
+# 3F) Interpret your plots and your statistical tests - what is going on?
+
