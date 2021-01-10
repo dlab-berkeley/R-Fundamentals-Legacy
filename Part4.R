30a256cf54ca9ce521058e11c186f92b669ca8a5
evan <evan@Evans-MacBook-Pro.local>
2021-01-09 21:28:09 -0800

    added challenge 5 links to R Markdown, bookdown, and flexdashboard

--- Part4.R
+++ Part4.R
@@ -452,3 +452,10 @@ plot(x = sims[,1], y = sims[,2],
 ##### 1. Automate something! Anything. (i.e., put a for-loop inside of a function and get some output)
 
 ##### 2. If this is too difficult, think about your own research - what might benefit from simple automation?
+
+##### Challenge 5 - going further with R Markdown, bookdown, and dashboards
+##### Recall that we have been going through R script files - these are just plain text files but with a .R extension. These are plain text files where we must enter a hashtag before text to enter it correctly, while entering code normally. However, R Markdown files are fully reproducible documents (with a .Rmd file extension) that mix text (entered normally and without the need for hashtags) and "chunks" of code (you can still hashtag comments inside a chunk of cocde). Open an R Markdown file by clicking File --> New File --> R Markdown... Give it a title, make sure the HTML button is selected, and click OK. 
+
+##### 1. Go through the RStudio tutorial to learn how to "knit" R Markdown files into HTML and other vibrant formats: https://rmarkdown.rstudio.com/
+##### 2. Furthermore, you can turn your R Markdown files into dynamic e-books! Go through yihui's excellent bookdown tutorial: https://bookdown.org/yihui/bookdown/get-started.html
+##### 3. Additionally, you can create fast dashboards by following this guide: https://rmarkdown.rstudio.com/flexdashboard/
--- solutions/Part 4 - challenge solutions.R
+++ solutions/Part 4 - challenge solutions.R
@@ -50,3 +50,10 @@ cylinders_function(height = cylinders$height,
 ##### Challenge 4 - automation
 ##### 1. Automate something! Anything. (i.e., put a for-loop inside of a function and get some output)
 ##### 2. If this is too difficult, think about your own research - what might benefit from simple automation?
+
+##### Challenge 5 - going further with R Markdown, bookdown, and dashboards
+##### Recall that we have so far been using R script files - these are just plain text files but with a .R extension. Here, we must enter a hashtag before text to enter it correctly, while entering code normally. However, R Markdown files are fully reproducible documents (with a .Rmd file extension) that mix text (entered normally and without the need for hashtags) and "chunks" of code (you can still hashtag comments inside a chunk of cocde) to create informative narratives for your research. Open an R Markdown file by clicking File --> New File --> R Markdown... Give it a title, make sure the HTML button is selected, and click OK. 
+
+##### 1. Go through the RStudio tutorial to learn how to "knit" R Markdown files into HTML and other vibrant formats: https://rmarkdown.rstudio.com/
+##### 2. Furthermore, you can turn your R Markdown files into dynamic e-books! Go through yihui's excellent bookdown tutorial: https://bookdown.org/yihui/bookdown/get-started.html
+##### 3. Additionally, you can create fast dashboards by following this guide: https://rmarkdown.rstudio.com/flexdashboard/
